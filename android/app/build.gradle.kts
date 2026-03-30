24fadbf4c6534f3705c8035f6ba04f8607229f4f
alefbs <alef.bispo.sefaz@gmail.com>
2026-03-30 00:27:24 -0300

    adicioando politica de provacidade e ajuste em build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,44 +1,44 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.lista_do_mercadinho"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    plugins {
+        id("com.android.application")
+        id("kotlin-android")
+        id("dev.flutter.flutter-gradle-plugin")
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
+    android {
+        namespace = "com.alefbs.mercadofacil"
+        compileSdk = flutter.compileSdkVersion
+        ndkVersion = flutter.ndkVersion
 
-    defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.alefbs.mercadofacil"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+
+        sourceSets {
+            getByName("main") {
+                java.srcDirs("src/main/kotlin")
+            }
+        }
+
+        defaultConfig {
+            applicationId = "com.alefbs.mercadofacil"
+            minSdk = flutter.minSdkVersion
+            targetSdk = flutter.targetSdkVersion
+            versionCode = flutter.versionCode
+            versionName = flutter.versionName
+        }
+
+        buildTypes {
+            release {
+                signingConfig = signingConfigs.getByName("release")
+            }
         }
     }
-}
 
-flutter {
-    source = "../.."
-}
+    flutter {
+        source = "../.."
+    }
